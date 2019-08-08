12e8006f50300fb0d22e8ea9b8982a78f1f9c832
flmngwllm <flmngwllm232@gmail.com>
2019-08-08 13:32:49 -0400

    created a navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Browser as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './'
 
 
 
