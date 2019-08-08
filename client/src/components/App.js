dfbfbe087541afb440cd03280cfbe6f486053100
flmngwllm <flmngwllm232@gmail.com>
2019-08-08 13:11:56 -0400

    added react router and added home route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Browser as Router, Route, Switch} from 'react-router-dom';
+
+
 
 class App extends React.Component{
     render(){
         return(
-            <div> App </div>
+            
+            <div> 
+                <Router>
+                    <div>
+                    <Switch>
+                        <Route path="/" component = {Home}/>
+                    </Switch>
+                    </div>
+                </Router>
+                 </div>
         )
     }
 }
