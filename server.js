157a8253661cc9477499f6a53eff5c7081a358cc
flmngwllm <flmngwllm232@gmail.com>
2019-08-07 23:18:42 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//importing middleware
 const express = require("express")
 const morgan = require("morgan")
 const app = express()
 
+//registering middleware
 app.use(morgan('dev'))
 app.use(express.json())
+
+//parse body for http requests from url string
 app.use(express.urlencoded({ extended : true}))
+
+//build directory for static resources
 app.use(express.static(`${__dirname}/client/build`))
 
 
@@ -13,7 +19,7 @@ app.get('/',  (req, res) => {
   })
 
 
-
+//setup prot and start port
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server is listening on PORT : ${PORT}`)
