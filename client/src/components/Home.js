12baf9898d4604e0a0f5d5d906d9f54fbf66a36f
flmngwllm <flmngwllm232@gmail.com>
2019-08-08 21:01:15 -0400

    added button that links to the search page, and added text on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                <h1>Brew This</h1>
+                <h1>The best brew you have had this week</h1>
+                <Link to="/Search"><button>Search</button></Link>
+
             </div>
         );
     }
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,10 @@ import {Link} from 'react-router-dom';
 const Nav = () => {
     return(
         <div>
+            <Link to="/"><h1>Brew This</h1></Link>
             <Link to="/">Home</Link>
+            <Link to="/Search">Search</Link>
+            <Link to="/Favorites">Favorites</Link>
         </div>
     )
 }
