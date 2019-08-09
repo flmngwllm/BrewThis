5856e1cc6a6796339bfa2622a0c2b211fee84f04
flmngwllm <flmngwllm232@gmail.com>
2019-08-09 04:35:14 -0400

    add css to the home component and imported it into the app

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,9 +5,17 @@ class Home extends Component {
     render() {
         return (
             <div id="Home">
-                <h1>Brew This</h1>
-                <h3>The best brew you have had this week</h3>
+                <div id="main-text">
+                <h1 id="name-text">Brew This</h1>
+                
+
+                <p id="sub-text">The best brew you have had this week</p>
+                </div>
+
+                <div id="entry">
                 <Link to="/BrewSearch"><button>Search</button></Link>
+                <Link to="/Favorites"><button>Favorites</button></Link>
+                </div>
 
             </div>
         );
