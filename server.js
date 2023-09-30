d4575e0648cfb8f703f6995ad37e624118203386
flmngwllm <flmngwllm232@gmail.com>
2023-09-29 23:13:06 -0400

    configuring vercel

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 //importing middleware
-const express = require("express")
-const morgan = require("morgan")
-const app = express()
-const routes = require("./routes/index")
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const routes = require("./routes/index");
 
 //registering middleware
-app.use(morgan('dev'))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
 //parse body for http requests from url string
-app.use(express.urlencoded({ extended : true}))
+app.use(express.urlencoded({ extended: true }));
 
 //build directory for static resources
-app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static(`${__dirname}/client/build`));
 
+app.use("/", routes);
 
-app.use('/', routes)
-
-app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/client/build/index.html`)
-  })
-
+app.get("/*", (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`);
+});
 
 //setup prot and start port
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () =>{
-    console.log(`Server is listening on PORT : ${PORT}`)
-})
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT : ${PORT}`);
+});
+
+module.exports = app;
