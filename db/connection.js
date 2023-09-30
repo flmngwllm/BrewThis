0f9d0b34f47902f4bb1adcca9a686421c3e7dbff
flmngwllm <flmngwllm232@gmail.com>
2023-09-30 00:28:35 -0400

    updated scripts

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connection.once('open', () => {
-    console.log("Mongoose has connected to MongoDB")
-})
+mongoose.connection.once("open", () => {
+  console.log("Mongoose has connected to MongoDB");
+});
 
-mongoose.connection.on('error', (error) => {
-    console.error(`MongoDB connection error!!! ${error}`)
-    process.exit(-1)
-})
+mongoose.connection.on("error", (error) => {
+  console.error(`MongoDB connection error!!! ${error}`);
+  process.exit(-1);
+});
 
-module.exports = mongoose
+module.exports = mongoose;
